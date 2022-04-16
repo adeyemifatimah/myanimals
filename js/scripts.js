fe4dfb8a6c4d76356b9aeeac8b3e8210efb328af
abisoye <beesoye111@gmail.com>
2022-04-16 21:13:10 +0100

    my jquery is now working

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $("#dont").click(function () {
+      $("#adjust1,#adjust2").hide(1000);
+      alert("my code works")
+    });
+
+  
+    
+    $("#pull").click(function () {
+        $("#adjust1,#adjust2").show(1000);
+        alert("i finally got this")
+      });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
