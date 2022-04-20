2e6e3028c2bdec9c5ffc44f538972860cc37ce7e
abisoye <beesoye111@gmail.com>
2022-04-20 14:01:33 +0100

    made some changes

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,36 @@ $(document).ready(function () {
       });
   
 
+      $("#ad1").click(function () {
+        $("#adjust1,#adjust2").show(1000);
+        alert("it is a butterfly")
+      });
+  
 
-
-
-
-
-
-
-
+      $("#ad2").click(function () {
+        $("#adjust1,#adjust2").show(1000);
+        alert("it is a leopard")
+      });
+  
+      $("#ad3").click(function () {
+        $("#adjust1,#adjust2").show(1000);
+        alert("it is a poddle dog")
+      });
+    
+      $("#ad4").click(function () {
+        $("#adjust1,#adjust2").show(1000);
+        alert("it is a labrador")
+      });   
+      
+      $("#ad5").click(function () {
+        $("#adjust1,#adjust2").show(1000);
+        alert("it is a poccupine ")
+      });   
+      
+      $("#ad6").click(function () {
+        $("#adjust1,#adjust2").show(1000);
+        alert("it is a lion/girl")
+      });
 
 
 
